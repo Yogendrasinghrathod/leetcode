067673f99aefe1de6a73f0287eaf92fe3a09c7ad
Yogendra Singh Rathod <127912809+Yogendrasinghrathod@users.noreply.github.com>
2025-04-22 11:00:49 +0530

    Time: 263 ms (55.41%), Space: 42.5 MB (57.26%) - LeetHub

---
+++ 0686-repeated-string-match/0686-repeated-string-match.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public int repeatedStringMatch(String a, String b) {
+        StringBuilder sb = new StringBuilder(a);
+        int count = 1;
+        while (sb.length() < b.length()) {
+            sb.append(a);
+            count++;
+        }
+        if (sb.toString().contains(b)) return count;
+        sb.append(a);
+        count++;
+        return sb.toString().contains(b) ? count : -1;
+    }
+}
