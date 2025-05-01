031f3746ab998fd2a3bf259a5a91bf2f883abe98
Yogendra Singh Rathod <127912809+Yogendrasinghrathod@users.noreply.github.com>
2025-05-01 22:23:16 +0530

    Time: 1 ms (42.27%), Space: 41.4 MB (13.48%) - LeetHub

--- 0258-add-digits/0258-add-digits.java
+++ 0258-add-digits/0258-add-digits.java
@@ -5,7 +5,7 @@ class Solution {
         
         while(num>=10){
             int sum=0;
-            while(num>0){
+            while(num!=0){
                 sum+=num%10;
                 num=num/10;
             }
